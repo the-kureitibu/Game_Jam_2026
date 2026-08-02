a13607d543e31be04a51945e27eb91d7eb75e995
Joshua Alcober <joshuaalcober2019@gmail.com>
2026-08-02 17:20:27 +0800

    Continue making enemy AI move

--- scenes/enemy_scene/enemy_scripts/mob_one.gd
+++ scenes/enemy_scene/enemy_scripts/mob_one.gd
@@ -4,7 +4,7 @@ extends EnemyBase
 #region Base Vars
 
 var health: float = 0.0
-var speed: float = 0.0
+var speed: float = 60.0
 var damage: float = 0.0
 
 
@@ -16,6 +16,14 @@ var to_left_max_dis: float = abs(-100.0)
 var to_right_max_dis: float = abs(100.0)
 var to_left_target: Vector2 = Vector2(-100.0, 0)
 var to_right_target: Vector2 = Vector2(100.0, 0)
+var to_right: bool = false
+var to_left: bool = false
+var left_dir: Vector2 = Vector2.LEFT
+var right_dir: Vector2 = Vector2.RIGHT
+
+var reached_right: bool = false
+var reached_left: bool = false
+var is_traveling: bool = false
 var starting_pos: Vector2 = Vector2.ZERO
 var facing_dir: int = 1
 
@@ -44,30 +52,68 @@ var facing_dir: int = 1
 
 #endregion
 #endregion 
+
+#region Tests 
+func _draw() -> void:
+	draw_circle(Vector2(0, 0), 200.0, Color.RED, false, -2.0)
+#endregion
+
 #region Processes
 
 func _ready() -> void:
 	starting_pos = global_position
-	#
-	#if t_player == null:
-		#"No target Player"
-		#return 
-	#
+	
+	print(starting_pos.distance_to(to_right_target), " starting pos distance to")
+	print(to_right_max_dis, " To right max dis")
+	
 func _physics_process(delta: float) -> void:
-	pass
+
+
+	velocity.x = 1.0 * speed 
+		
+	move_and_slide()
 
 func handle_movement(delta) -> void:
 	if mob_one_state == MobStates.ATTACKING or mob_one_state == MobStates.HURT:
 		return 
+	
+	start_idle_moving(delta)
 
 func start_idle_moving(delta) -> void:
-	if !mob_one_state == MobStates.IDLE:
-		return 
+	print("Got here?")
+	#if !mob_one_state == MobStates.IDLE:
+		#return 
+	#
+
 	
-	if facing_dir == 1:
-		if global_position.distance_to(to_right_target) < to_right_max_dis:
-			var signed_dir = global_position.direction_to(to_right_target)
+	#if facing_dir == 1 and !reached_right:
+		#print('second line worked')
+		#if starting_pos.distance_to(to_right_target) < to_right_max_dis:
+			#print(starting_pos.distance_to(to_right_target), " starting pos distance to")
+			#print(to_right_max_dis, " To right max dis")
+			#
+			#var signed_dist = global_position.distance_to(to_right_target)
+			#var signed_dir = sign(signed_dist)
+			#
+	#
 			
+		
+
+func decide_side() -> void:
+	var signed_dist = global_position.distance_to(to_right_target)
+	var signed_dir = sign(signed_dist)
+	
+	if signed_dist <= to_right_max_dis:
+		is_traveling = false
+		reached_right = true
+		#sprite flip
+		facing_dir = signed_dir
+
+	elif signed_dist <= to_left_max_dis:
+		is_traveling = false
+		reached_left = true
+		#sprite flip
+		facing_dir = signed_dir
 
 #endregion
 	
