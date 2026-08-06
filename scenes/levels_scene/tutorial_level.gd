9873d143e2d263ddd68780cdaace369ac9280193
Joshua Alcober <joshuaalcober2019@gmail.com>
2026-08-07 02:19:09 +0800

    Continue working on tutorial level

--- scenes/levels_scene/tutorial_level.gd
+++ scenes/levels_scene/tutorial_level.gd
@@ -9,6 +9,22 @@ extends Node2D
 @onready var tutorial_jump: Sprite2D = $TutorialJump
 @onready var tutorial_rage: Sprite2D = $TutorialRage
 
+@onready var arrow_one: Sprite2D = $ArrowOne
+@onready var arrow_two: Sprite2D = $ArrowTwo
+@onready var arrow_three: Sprite2D = $ArrowThree
+@onready var arrow_four: Sprite2D = $ArrowFour
+
+#region Collision Shapes
+
+@onready var arrow_one_col: CollisionShape2D = $ArrowOneArea/ArrowOneCol
+@onready var arrow_two_col: CollisionShape2D = $ArrowTwoArea/ArrowTwoCol
+@onready var arrow_three_col: CollisionShape2D = $ArrowThreeArea/ArrowThreeCol
+@onready var arrow_four_col: CollisionShape2D = $ArrowFourArea/ArrowFourCol
+
+
+#endregion
+
+
 #endregion
 
 
@@ -25,3 +41,22 @@ func _on_exit_area_body_entered(body: Node2D) -> void:
 
 	if player:
 		print("Move to main scene")
+
+#region Area Signals
+
+func _on_arrow_one_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_arrow_two_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_arrow_three_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+
+func _on_arrow_four_area_body_entered(body: Node2D) -> void:
+	pass # Replace with function body.
+
+#endregion
