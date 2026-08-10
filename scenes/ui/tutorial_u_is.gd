7579f13e4a185ec0ef0a90bfb682d778531c64ba
Joshua Alcober <joshuaalcober2019@gmail.com>
2026-08-11 06:37:51 +0800

    Continue working on tutorial UI scene

--- scenes/ui/tutorial_u_is.gd
+++ scenes/ui/tutorial_u_is.gd
@@ -42,6 +42,56 @@ extends Control
 
 #endregion
 
+#region First Panel Images Container
+
+@onready var main_attk_image: HBoxContainer = $MainMargin/BasicAttackPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainAttkImage
+@onready var main_combo_image: HBoxContainer = $MainMargin/BasicAttackPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainComboImage
+@onready var main_skl_1_image: HBoxContainer = $MainMargin/BasicAttackPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainSkl1Image
+@onready var main_skl_2_image: HBoxContainer = $MainMargin/BasicAttackPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainSkl2Image
+
+#endregion
+
+#region Second Panel Images Container
+
+@onready var main_jump_image: HBoxContainer = $MainMargin/BlockJumpPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainJumpImage
+@onready var main_block_image: HBoxContainer = $MainMargin/BlockJumpPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainBlockImage
+
+#endregion
+
+#region Third Panel Images Container
+
+@onready var main_r_attk_image: HBoxContainer = $MainMargin/RagePanel/MainVBox/BG/ImageVBContainer/BackDrop/MainRAttkImage
+
+#endregion
+
+#region Fourth Panel Images Container
+@onready var main_r_skl_1_image: HBoxContainer = $MainMargin/RageSkillPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainRSkl1Image
+@onready var main_r_skl_2_image: HBoxContainer = $MainMargin/RageSkillPanel/MainVBox/BG/ImageVBContainer/BackDrop/MainRSkl2Image
+
+#endregion
+
+#region Images Array
+
+@onready var panel_one_images: Array = [
+	main_attk_image,
+	main_combo_image,
+	main_skl_1_image,
+	main_skl_2_image
+]
+
+@onready var panel_two_images: Array = [
+ 	main_jump_image,
+	main_block_image
+]
+
+@onready var panel_four_images: Array = [
+	main_r_skl_1_image,
+	main_r_skl_2_image
+]
+
+#endregion
+
+
 #region Identifiers
 
 @onready var current_index := 0
@@ -49,7 +99,7 @@ var is_attk_tutorial_panel := false
 var is_jump_block_panel := false
 var is_rage_panel := false
 var is_rage_skill_panel := false
-
+var is_panel_open := false
 
 #endregion
 
@@ -65,7 +115,7 @@ var is_rage_skill_panel := false
 	"block_jump_panel": {
 
 		"jump_tut": "Press Space Bar to jump",
-		"block_tut": "Press Right Mouse button to block",
+		"block_tut": "Press Right Mouse button to block. Bucko automatically teleports in front of Ami",
 	},
 	
 	"rage_panel": {
@@ -84,14 +134,14 @@ var is_rage_skill_panel := false
 #endregion 
 
 func _ready() -> void:
-	#arrow_navs_panel.visible = false
-	#
-	#for panel in panel_arrays:
-		#panel.visible = false
+	arrow_navs_panel.visible = false
+	
+	for panel in panel_arrays:
+		panel.visible = false
 	#open_attk_tutorial_panel()
 	#open_jump_block_panel()
 	#open_rage_panel()
-	open_rage_skill_panel()
+	#open_rage_skill_panel()
 
 func _process(delta: float) -> void:
 	pass
@@ -100,24 +150,52 @@ func _process(delta: float) -> void:
 #region Open Panels 
 
 func open_attk_tutorial_panel() -> void:
+	if is_panel_open:
+		return
+	
+	if is_attk_tutorial_panel:
+		return
+		
+	is_panel_open = true 
 	is_attk_tutorial_panel = true
 	
 	panel_ini_helper(attk_tutorial_panel, arrow_navs_panel, 
 					text_label_one, 0, current_index)
 	
 func open_jump_block_panel() -> void:
+	if is_panel_open:
+		return
+	
+	if is_jump_block_panel:
+		return
+		
+	is_panel_open = true 
 	is_jump_block_panel = true
 
 	panel_ini_helper(jump_block_panel, arrow_navs_panel, 
 				text_label_two, 1, current_index)
 
 func open_rage_panel() -> void:
+	if is_panel_open:
+		return
+	
+	if is_rage_panel:
+		return
+		
+	is_panel_open = true 
 	is_rage_panel = true
 	
 	panel_ini_helper(rage_panel, arrow_navs_panel, 
 				text_label_three, 2, current_index)
 
 func open_rage_skill_panel() -> void:
+	if is_panel_open:
+		return
+	
+	if is_rage_skill_panel:
+		return
+		
+	is_panel_open = true 
 	is_rage_skill_panel = true
 	
 	panel_ini_helper(rage_skill_panel, arrow_navs_panel, 
@@ -145,7 +223,30 @@ func panel_ini_helper(panel: MarginContainer, arrow_panel: VBoxContainer,
 
 
 func _on_arrow_left_button_pressed() -> void:
-	pass # Replace with function body.
+	
+	var current_panel: MarginContainer
+	var current_image_array: Array
+	var current_image_panel: HBoxContainer
+	var _index: int = 0
+	
+
+	if is_attk_tutorial_panel:
+		current_panel = attk_tutorial_panel
+		current_image_array = panel_one_images
+		current_image_panel = current_image_array[_index]
+	elif is_jump_block_panel:
+		current_panel = jump_block_panel
+		current_image_array = panel_two_images
+		current_image_panel = current_image_array[_index]
+	elif is_rage_panel:
+		current_panel = rage_panel
+		current_image_panel = main_r_attk_image
+	elif is_rage_skill_panel:
+		current_panel = rage_skill_panel 
+		current_image_array = panel_four_images
+		current_image_panel = current_image_array[_index]
+
+
 
 
 func _on_arrow_right_button_pressed() -> void:
@@ -157,6 +258,16 @@ func _on_arrow_right_button_pressed() -> void:
 #region Exit Button
 
 func _on_exit_button_pressed() -> void:
-	pass # Replace with function body.
+	is_panel_open = false
 	
+	for panel in panel_arrays:
+		panel.visible = false
+
+	current_index = 0
+	is_attk_tutorial_panel = false
+	is_jump_block_panel = false
+	is_rage_panel = false
+	is_rage_skill_panel = false
+	is_panel_open = false
+
 #endregion
