7bbf39845020b025e51fc29443ff21259bbac848
Joshua Alcober <joshuaalcober2019@gmail.com>
2026-08-25 10:55:19 +0800

    Work on epilogue

--- scenes/ui/epilogue_scene.gd
+++ scenes/ui/epilogue_scene.gd
@@ -4,40 +4,27 @@ extends Control
 
 #region References 
 
+@onready var bg_one: TextureRect = $BGOne
+@onready var bg_two: TextureRect = $BGTwo
+
+
 @onready var amiya_thumbnail_path: String = "res://assets/sprites/ui/thumbnails/Amiya_thumbnail.png"
 @onready var bucko_thumbnail_path: String = "res://assets/sprites/ui/thumbnails/bucko_thumbnail.png"
-@onready var boss_thumbnail1_path: String = "res://assets/sprites/ui/thumbnails/boss_thumbnail.png"
-@onready var boss_thumbnail2_path: String = "res://assets/sprites/ui/thumbnails/boss_thumbnailv1.png"
-@onready var boss_thumbnail3_path: String = "res://assets/sprites/ui/thumbnails/boss_thumbnailv3.png"
-@onready var boss_thumbnail4_path: String = "res://assets/sprites/ui/thumbnails/boss_thumbnailv4.png"
-@onready var boss_thumbnail4v1_path: String = "res://assets/sprites/ui/thumbnails/boss_thumbnailv4v2.png"
 
-
-
-@onready var amiya_bucko_image: TextureRect = $MainMargin/ImageVBox/HBoxContainer/AmiyaBuckoImage
-@onready var boss_image: TextureRect = $MainMargin/ImageVBox/HBoxContainer/BossImage
-
+@onready var amiya_image: TextureRect = $MainMargin/ImageVBox/HBoxContainer/AmiyaImage
+@onready var bucko_image: TextureRect = $MainMargin/ImageVBox/HBoxContainer/BuckoImage
 
 @onready var main_margin: MarginContainer = $MainMargin
 @onready var panel_container: PanelContainer = $MainMargin/PanelContainer
 
 @onready var player_name_container: VBoxContainer = $MainMargin/PanelContainer/MainHbox/PlayerNameContainer
-@onready var boss_name_container: VBoxContainer = $MainMargin/PanelContainer/MainHbox/BossNameContainer
+@onready var player2_name_container: VBoxContainer = $MainMargin/PanelContainer/MainHbox/BuckoNameContainer
 @onready var v_box_container: VBoxContainer = $MainMargin/PanelContainer/SecondHBox/VBoxContainer
 
 @onready var player_name_label: RichTextLabel = $MainMargin/PanelContainer/MainHbox/PlayerNameContainer/PlayerNameLabel
-@onready var boss_name_label: RichTextLabel = $MainMargin/PanelContainer/MainHbox/BossNameContainer/BossNameLabel
+@onready var bucko_name_label: RichTextLabel = $MainMargin/PanelContainer/MainHbox/BuckoNameContainer/BuckoNameLabel
 @onready var dialogue_text_label: RichTextLabel = $MainMargin/PanelContainer/SecondHBox/VBoxContainer/DialogueTextLabel
 @onready var next_text_label: RichTextLabel = $MainMargin/PanelContainer/NextTextContainer/NextTextLabel
-
-#region Boss Animation
-
-@onready var boss_anim_player: AnimatedSprite2D
-@onready var boss_main_collision: CollisionShape2D
-@onready var boss_main_hurt_box: CollisionShape2D
-@onready var boss_target = get_tree().get_first_node_in_group("Boss_target")
-
-#endregion -- Boss Animation
 
 #endregion -- References 
 
@@ -45,20 +32,13 @@ extends Control
 
 var current_index: int = 0
 
-const MAX_INDEX: int = 10
+const MAX_INDEX: int = 11
 
 var has_ended_dialogue: bool = false 
 var start_dialogue: bool = false
 
 #endregion -- Base
 
-#region Gate Keepers
-
-@onready var is_demon_lord: bool = false
-@onready var is_transition: bool = false
-@onready var transforming_done: bool = false
-
-#endregion -- Gate Keepers
 
 #region Dialogue and Image Collection
 
@@ -78,18 +58,6 @@ var dialogue_lines: Array[Dictionary] = [
 		"image": amiya_thumbnail_path
 	},
 	{
-		"speaker": "Salaryman Satou, Four Heavenly Kings",
-		"name": "Salaryman Satou, four heavenly kings",
-		"text": [
-			"Oh no. Don't kill me just yet.",
-			"Haha... そう Spidor. そう!!!",
-			"That's right.",
-			"No matter. I'll start with you and take care of the rats after."
-		],
-		"side": "right",
-		"image": boss_thumbnail1_path
-	},
-	{
 		"speaker": "Bucko",
 		"name": "Bucko",
 		"text": [
@@ -106,15 +74,16 @@ var dialogue_lines: Array[Dictionary] = [
 
 @onready var dialogue_sequences: Array[Dictionary] = [
 	{"speaker": 0, "line": 0}, 
-	{"speaker": 2, "line": 0}, 
 	{"speaker": 1, "line": 0}, 
 	{"speaker": 0, "line": 1}, 
+	{"speaker": 1, "line": 1}, 
 	{"speaker": 0, "line": 2},
-	{"speaker": 1, "line": 1},
-	{"speaker": 0, "line": 3},
 	{"speaker": 1, "line": 2},
-	{"speaker": 1, "line": 3},
+	{"speaker": 0, "line": 3},
 	{"speaker": 0, "line": 4},
+	{"speaker": 1, "line": 3},
+	{"speaker": 0, "line": 5},
+	{"speaker": 1, "line": 4},
 ]
 
 @onready var names_and_text_collection: Array = [
@@ -137,18 +106,6 @@ var dialogue_lines: Array[Dictionary] = [
 
 #region Processes
 
-func _enter_tree() -> void:
-	if !boss_target:
-		boss_target = get_tree().get_first_node_in_group("Boss_target")
-	
-	boss_anim_player = boss_target.d_sprite
-	boss_main_collision = boss_target.main_col
-	boss_main_hurt_box = boss_target.hurt_col
-	
-	if !SignalHub.ready_for_second_phase.is_connected(update_boss_form):
-		SignalHub.ready_for_second_phase.connect(update_boss_form)
-
-
 func _ready() -> void:
 
 	dialogue_helper(0, "left", 0)
@@ -157,18 +114,7 @@ func _ready() -> void:
 	next_text_label.text = names_and_text_collection[3]
 	pulse_control(next_text_label)
 
-
-func _process(delta: float) -> void:
-	pass
-	
 #endregion -- Processes
-
-#region Dialogue State related
-
-func update_boss_form() -> void:
-	is_demon_lord = true
-
-#endregion -- Dialogue State related
 
 
 #region Advancing Dialogue
@@ -185,21 +131,18 @@ func sequences_helper(curr_index: int, speaker: String = "speaker", line: String
 	return [speaker_index, line_index]
 
 func advance_dialogue() -> void:
-	if current_index == 3 and !transforming_done:
-		transition_to_demon_lord()
-	
-	if is_transition:
-		return
-	
+
 	if has_ended_dialogue:
 		return
 
-
 	if current_index == MAX_INDEX:
 		has_ended_dialogue = true
-		SignalHub.mid_fight_dialogue_done.emit()
+
+		panel_container.visible = false
+		amiya_image.visible = false
+		bucko_image.visible = false
 		
-		queue_free()
+		show_end_buttons()
 		return
 	
 	var sequence_index = sequences_helper(current_index)
@@ -213,8 +156,6 @@ func advance_dialogue() -> void:
 			side = "left"
 		1:
 			side = "right"
-		2:
-			side = "left"
 	
 	dialogue_helper(sequence_speaker_index, side, sequence_line_index)
 	
@@ -231,23 +172,12 @@ func dialogue_helper(main_index: int, side: String, cur_index: int, sp_text_indx
 	match speaker_name:
 		"Amiya Aranha":
 			player_name_label.text = "Amiya Aranha"
-			amiya_bucko_image.texture = load(amiya_thumbnail_path)
-		"Salaryman Satou, Four Heavenly Kings":
-			if cur_index == 0:
-				boss_name_label.text = "Salaryman Satou, Four Heavenly Kings"
-				boss_image.texture = load(boss_thumbnail1_path)
-			elif cur_index == 1:
-				boss_name_label.text = "Salaryman Satou, Demon Lord"
-				boss_image.texture = load(boss_thumbnail4_path)
-			elif cur_index == 2:
-				boss_name_label.text = "Salaryman Satou, Demon Lord"
-				boss_image.texture = load(boss_thumbnail4v1_path)
-			else:
-				boss_name_label.text = "Salaryman Satou, Demon Lord"
-				boss_image.texture = load(boss_thumbnail4_path)
+			amiya_image.texture = load(amiya_thumbnail_path)
 		"Bucko":
 			player_name_label.text = "Bucko"
-			amiya_bucko_image.texture = load(bucko_thumbnail_path)
+			bucko_image.texture = load(bucko_thumbnail_path)
+			if cur_index == 4:
+				show_bg_two()
 	
 	match side:
 		"left":
@@ -258,60 +188,25 @@ func dialogue_helper(main_index: int, side: String, cur_index: int, sp_text_indx
 	
 	dialogue_text_label.text = speaker_text_collection[cur_index]
 	
-func transition_to_demon_lord() -> void:
-	if is_transition:
-		return
-	
-	is_transition = true
-	boss_anim_player.visible = true 
-
-	handle_first_transform()
-	
-	SignalHub.is_needed_flip.emit()
-
-
-func pass_boss_col_shape_values(radius: float, height: float, pos: Vector2, col_shape: CollisionShape2D):
-
-	col_shape.shape.radius = radius
-	col_shape.shape.height = height
-	col_shape.position = pos 
-	
-func set_final_thumbnail() -> void:
-	boss_name_label.text = "Salaryman Satou, Demon Lord"
-	boss_image.texture = load(boss_thumbnail4v1_path)
-	
-	is_transition = false
-	transforming_done = true
-
-#region Transitions Animations Related
-
-func handle_first_transform() -> void:
-	boss_anim_player.play("to_demon_one")
-	await boss_anim_player.animation_finished
-	
-	handle_shape_and_offset()
-	
-	handle_last_transform()
-	
-	
-func handle_last_transform() -> void:
-	boss_anim_player.play("to_demon_two")
-	
-	await boss_anim_player.animation_finished
-	
-	set_final_thumbnail()
-	
-func handle_shape_and_offset() -> void:
-	
-	boss_anim_player.offset = Vector2(0, -100.0)
-	
-	pass_boss_col_shape_values(19, 160, Vector2(0, -102.0), boss_main_collision)
-	pass_boss_col_shape_values(19, 160, Vector2(0, -102.0), boss_main_hurt_box)
-
-
-#endregion -- Transitions Animation
-
 #endregion -- Advancing Dialogue
+
+#region Backgrounds
+
+func show_bg_two() -> void:
+	bg_one.visible = false
+	print("BG one visible?: ", bg_one.visible)
+	bg_two.visible = true
+	print("BG two visible?: ", bg_two.visible)
+	
+#endregion -- Backgrounds
+
+
+#region End Buttons
+
+func show_end_buttons() -> void:
+	print("Show buttons")
+
+#endregion -- End Buttons
 
 #region Effects 
 
